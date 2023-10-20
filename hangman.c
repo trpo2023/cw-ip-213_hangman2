ecca28a61f442a0e8132c44e6b481711740ba776
Ракшун Сергей <Rakshun2004@gmail.com>
2023-10-20 23:59:02 +0700

    f

--- hangman.c
+++ hangman.c
@@ -1,11 +1,4 @@
 #include "hangman.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-#define MAX_LENGTH 20
-#define MAX_TRIES 6
 
 void display_hangman(int tries) {
     printf("Рисуем виселицу...\n\n");
--- hangman.h
+++ hangman.h
@@ -1,5 +1,11 @@
 #ifndef HANGMAN_H
 #define HANGMAN_H
+#define MAX_LENGTH 20
+#define MAX_TRIES 6
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 void display_hangman(int tries);
 int string_length(char* string);
